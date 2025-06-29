0521f7c15979075ce46fb0be52751d6417aa3651
Abhay Kumar <163289874+BismarckSVBP@users.noreply.github.com>
2025-06-29 22:02:47 +0530

    Update generateTokenAndSetCookie.js

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -7,7 +7,7 @@ export const generateTokenAndSetCookie = (res, userId) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production", // required for SameSite=None
+    secure: true, // required for SameSite=None
     sameSite: "None", // ✅ allow cookie to be sent in cross-site requests
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
